ff5428c4bc759fce124821056208aabf16477231
andvargrad@gmail.com <andvargard@gamil.com>
2023-03-02 23:05:46 +0300

    Доработал фильтрацию номеров

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import re
 import requests
 import csv
 
-numbers =[]
-
+numbers = []
+numbers1 = []
 with open('urls.csv') as f:
     reader = csv.reader(f)
     urls = [row[0] for row in reader]
@@ -19,7 +19,14 @@ for i in final_list:
     len_list = len(i)
     if len_list == 11 or len_list == 12:
         numbers.append(i)
-print(f'Телефоны: - {numbers}')
+
+for j in numbers:
+    if j[0] == "+" or j[0] == "7" or j[0] == "8":
+        numbers1.append(j)
+
+
+
+print(f'Телефоны: - {numbers1}')
 print(f'Почта: - {emails}')
 
 
