706700dea68128dbf770997cf3e625df6ef8aa01
andvargrad@gmail.com <andvargard@gamil.com>
2023-03-09 18:38:43 +0300

    Полностью переработал программу

--- main.py
+++ main.py
@@ -1,42 +1,67 @@
+from bs4 import BeautifulSoup
 import re
 import requests
-import csv
+import fake_useragent
 
-numbers = []
-numbers1 = []
-with open('urls.csv') as f:
-    reader = csv.reader(f)
-    urls = [row[0] for row in reader]
+ua = fake_useragent.UserAgent()
+headers = {
+    "User-Agent": ua.random
+}
 
-for url in urls:
-    response = requests.get(url)
-    emails = set(re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', response.text))
+with open('C:\\Users\\user\\Desktop\\python\\parser\\link.txt', 'r') as file:
+    content = file.readlines()
+    my_list = [line.strip() for line in content]
+work_list  = []
+# удаление дубликатов из списка
+my_list = list(set(my_list))
+my_list = list(filter(lambda x: x != '', my_list))
+for url in my_list:
+    list_url = url
+    work_list.append(url)
 
-    phones = set(re.findall(r'(\+?\d[\d\-\s\(\)]{8,}\d)', response.text))
-    final_list = [phone.translate(str.maketrans('', '', ',()- ')) for phone in phones]
+def extract_emails(soup):  # пытается найти все email в HTML коде страницы
+    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    find_emails_pattern = re.findall(email_pattern, str(soup))
+    unique_emails = list(set(find_emails_pattern))
+    return unique_emails
 
-for i in final_list:
-    len_list = len(i)
-    if len_list == 11 or len_list == 12:
-        numbers.append(i)
-
-for j in numbers:
-    if j[0] == "+" or j[0] == "7" or j[0] == "8":
-        numbers1.append(j)
-
-
-with open("numbers.txt", 'a') as f:
-    for number in numbers1:
-        f.write(number + "\n")
-    f.close()
-
-with open("email.txt", 'a') as f:
-    for email in emails:
-        f.write(email + "\n")
-    f.close()
+    # ищет номер
 
 
-print(f'Телефоны: - {set(numbers1)}')
-print(f'Почта: - {emails}')
+def phone_numbers_extraction(soup):
+    # пытается найти все номера телефонов в HTML коде странице
+    # регулярное выражение для поиска номеров телефонов
+    phone_pattern = r'\+?\d[\d\-\s()]{8,}\d'
+    # поиск совпадений номера по переменной
+    find_phones_pattern = re.findall(phone_pattern, soup)
+    # удаляем лишние знаки
+    deletion_of_signs = [phone.replace(' ', '').replace('-', '').replace('(', '').replace(')', '') for phone in
+                         find_phones_pattern]
+    # удаляем дубликаты номеров
+    dell_dabl = list(set(deletion_of_signs))
+    # удаляем + в начале строки
+    dell_ = [number.replace('+', '') for number in dell_dabl]
+    # удаляет все кроме тех которые начинаются на 7 и 8
+    dell_new = [x for x in dell_ if x.startswith(('7', '8'))]
+    # удаляются все которые меньше 10
+    dell_new = [x for x in dell_new if len(x) > 10]
+    # остаются те которые меньше или равно 11
+    dell_new = [x for x in dell_new if len(x) <= 11]
+    # удаляет дубли
+    dell_new = list(set(dell_new))
+    # возвращает список уникальных номеров телефонов
+    # print(find_phones_pattern)   # проверочный принт
+    return dell_new
 
 
+
+for url in my_list:
+    sent = requests.get(url, headers={"User-Agent": ua.random})
+    soup = BeautifulSoup(sent.text, 'lxml')
+
+    emails = extract_emails(soup)
+    phones = phone_numbers_extraction(str(soup))
+
+    print("URL:", url)
+    print("Emails found:", emails)
+    print("Phone numbers found:", phones)
